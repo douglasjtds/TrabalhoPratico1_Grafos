3125aa28197fcf0f143a51ec3fa1622cc5911ce6
douglasjtds <douglasjtds@gmail.com>
2017-10-21 19:39:25 -0200

    trying to read file (with errors)

--- TP1_Grafos/src/tp1_grafos/TP1_Grafos.java
+++ TP1_Grafos/src/tp1_grafos/TP1_Grafos.java
@@ -1,5 +1,9 @@
 package tp1_grafos;
 
+import java.io.File;
+import java.io.FileInputStream;
+import sun.misc.IOUtils;
+
 /**
  *
  * @author douglasjtds & yMetsBless
@@ -12,5 +16,38 @@ public class TP1_Grafos {
     public static void main(String[] args) {
         // TÁ, E AGR?
         //UCYSDCHSDUHCSUDHCUSDUCHUISADCHIUSHDAIUCHSD
+        
+        String path = args[0];
+        File CP_file = new File(path);
+        path = "TrabalhoPratico1_Grafos\ArestasFacebook_20171010214533.txt";
+        
+        
+        File fileToRead = new File(path);
+
+        try( FileReader fileStream = new FileReader( fileToRead ); 
+        BufferedReader bufferedReader = new BufferedReader( fileStream ) ) {
+
+        String line = null;
+
+        while( (line = bufferedReader.readLine()) != null ) {
+            //do something with line
+        }
+
+        } catch ( FileNotFoundException ex ) {
+            //exception Handling
+        } catch ( IOException ex ) {
+            //exception Handling
+        }
+        
+        
+        
+        ///
+        try(FileInputStream inputStream = new FileInputStream("TrabalhoPratico1_Grafos\ArestasFacebook_20171010214533.txt")) {     
+            String everything = IOUtils.toString(inputStream);
+            // do something with everything string
+        } catch (Exception couldNotReadFileException){
+            System.out.println("Arquivo não pôde ser lido.");
+        }
+        
     }
 }
