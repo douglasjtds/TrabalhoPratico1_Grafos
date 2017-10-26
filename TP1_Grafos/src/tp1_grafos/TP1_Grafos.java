e4f4a73776e811aa42af8bbb46277fd2f25621d2
Matheus Pires <matheuswith51@hotmail.com>
2017-10-26 01:45:11 -0200

    Agora lê o .txt
    Le o .txt e joga na string[]

--- TP1_Grafos/src/tp1_grafos/TP1_Grafos.java
+++ TP1_Grafos/src/tp1_grafos/TP1_Grafos.java
@@ -1,12 +1,10 @@
 package tp1_grafos;
 
-//import java.io.File;
-//import java.io.FileInputStream;
-//import sun.misc.IOUtils;
-
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Node;
@@ -16,56 +14,67 @@ import org.graphstream.graph.implementations.SingleGraph;
  *
  * @author douglasjtds & yMetsBless
  */
+
 public class TP1_Grafos {
+    
+        public static String[] InicializarArquivo(String caminho, int linhasArquivo) throws IOException{
+            String[] vetorPreenchido = new String[linhasArquivo];
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        
-//              CÓDIGO DOUGLAS
+            FileReader arquivoInicial = new FileReader(caminho);
+            BufferedReader LeitorTxt = new BufferedReader(arquivoInicial);
 
-//        String path = args[0];
-//        File CP_file = new File(path);
-//        path = "TrabalhoPratico1_Grafos\ArestasFacebook_20171010214533.txt";
-//        
-//        
-//        File fileToRead = new File(path);
-//
-//        try( FileReader fileStream = new FileReader( fileToRead ); 
-//        BufferedReader bufferedReader = new BufferedReader( fileStream ) ) {
-//
-//        String line = null;
-//
-//        while( (line = bufferedReader.readLine()) != null ) {
-//            //do something with line
-//        }
-//
-//        } catch ( FileNotFoundException ex ) {
-//            //exception Handling
-//        } catch ( IOException ex ) {
-//            //exception Handling
-//        }
-//        
-//        
-//        
-//        ///
-//        try(FileInputStream inputStream = new FileInputStream("TrabalhoPratico1_Grafos\ArestasFacebook_20171010214533.txt")) {     
-//            String everything = IOUtils.toString(inputStream);
-//            // do something with everything string
-//        } catch (Exception couldNotReadFileException){
-//            System.out.println("Arquivo não pôde ser lido.");
-//        }
- 
-           String EdgePath = "TrabalhoPratico1_Grafos\\ArestasFacebook_20171010214533.txt";
-           String VertexPath = "TrabalhoPratico1_Grafos\\VerticesFacebook_20171010214551.txt";
-           
-           int qntdArestas = 41161;
-           int qntdVertices = 1919;
-           Arquivo teste = new Arquivo(EdgePath, VertexPath);
+                for(int i=0; i<linhasArquivo; i++){
+                    vetorPreenchido[i] = LeitorTxt.readLine();
+                }
+
+            arquivoInicial.close();
+            return vetorPreenchido;        
+        }
+
+        //      POSSÍVEL CLASSE PRA CONTAR QUANTAS LINHAS TEM O FILE.TXT. TEM QUE MEXER.
+        //    
+        //    public int qntdLinhas(String arquivoPath) throws IOException{
+        //        
+        //        BufferedReader reader = new BufferedReader(new FileReader(arquivoPath));
+        //            int lines = 0;
+        //                while (reader.readLine() != null){
+        //                    lines++;
+        //                }
+        //            reader.close();
+        //            
+        //            return lines;
+        //    } 
+        
+        public static void main(String[] args) {
             
-//            teste.InicializarArquivo(EdgePath, qntdArestas);      ERRO DE TRY CATCH
-//            teste.InicializarArquivo(VertexPath, qntdVertices);   ERRO DE TRY CATCH
-    }
+            Path EdgePath = Paths.get("C:\\Users\\mathe\\Documents\\DOCUMENTS\\FACULDADE\\GitHub\\TrabalhoPratico1_Grafos\\ArestasFacebook_20171010214533.txt");
+            Path VertexPath = Paths.get("C:\\Users\\mathe\\Documents\\DOCUMENTS\\FACULDADE\\GitHub\\TrabalhoPratico1_Grafos\\VerticesFacebook_20171010214551.txt");         
+            
+            String CaminhoAresta = EdgePath.toString();
+            String CaminhoVertice = VertexPath.toString();
+
+            int qntdArestas = 41161;
+            int qntdVertices = 1919;
+            
+            String[] vetorArestas, vetorVertices;
+            
+                try {
+                   vetorArestas = InicializarArquivo(CaminhoAresta, qntdArestas);
+                   vetorVertices = InicializarArquivo(CaminhoVertice, qntdVertices);
+
+                    for (int i=0; i<qntdVertices; i++){
+                       System.out.println(vetorVertices[i]);
+                    }    
+
+                    System.out.println("\n\n\n");
+
+                    for (int i=0; i<qntdArestas; i++){
+                       System.out.println(vetorArestas[i]);
+                    } 
+                }
+                catch (IOException arq){
+                    System.out.println(arq.getMessage());
+                }
+            
+        }
 }
